e7ebf6ca1c4afc2dd00e0748fdaf612760a9654d
Abdulmajid Ali <TIOTECH1998@GMAIL.COM>
2022-07-14 00:55:35 +0400

    1.3

--- main.tf
+++ main.tf
@@ -1,17 +1,19 @@
 resource "google_compute_instance" "server" {
   project = "my-cloud-resume-349920"
   name = "mycloudresume"
-  zone = "us-central1-a"
   machine_type = "e2-micro"
+  zone = "us-central1-a"
   boot_disk {
     initialize_params {
-image = "ubuntu-2204-jammy-v20220706"
-    size= 10
-    type= "pd_standard"
+      image = "ubuntu-2204-jammy-v20220706"
+      size= 10
+      type= "pd-standard"
     }
   } 
   network_interface {
     network = "default"
+    access_config {
+     }
     
-  }
+    }
   } 
