63ffa19c1b002efb702828e210e8e84a88286601
Abdulmajid Ali <TIOTECH1998@GMAIL.COM>
2022-07-14 03:25:50 +0400

    1.4

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
 resource "google_compute_instance" "server" {
-  project = "my-cloud-resume-349920"
+  project = "qwiklabs-gcp-01-cb5b498da450"
   name = "mycloudresume"
   machine_type = "e2-micro"
   zone = "us-central1-a"
+  tags = ["http-server","https-server"]
+
   boot_disk {
     initialize_params {
       image = "ubuntu-2204-jammy-v20220706"
@@ -16,4 +18,20 @@ resource "google_compute_instance" "server" {
      }
     
     }
-  } 
+  }
+
+  resource "google_compute_firewall" "default" {
+  name    = "server-firewall"
+  network = "default"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "8080"]
+  }
+  source_tags = ["web"]
+  target_tags = ["server"] #The target tag defines the Google Cloud VMs to which the rule applies.
+  }
